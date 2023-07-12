21af8c897690e498cbb3ec1d01426434affc4bdd
frisitano <francesco.risitano95@gmail.com>
2023-07-12 11:39:01 +0100

    update

---
+++ miden-tx/src/mock.rs
@@ -0,0 +1,64 @@
+use super::{
+    Account, AccountId, BlockHeader, ChainMmr, DataStore, DataStoreError, Note, NoteOrigin,
+};
+use crypto::StarkField;
+use miden_objects::mock::mock_inputs;
+
+#[derive(Clone)]
+pub struct MockDataStore {
+    pub account: Account,
+    pub block_header: BlockHeader,
+    pub block_chain: ChainMmr,
+    pub notes: Vec<Note>,
+}
+
+impl MockDataStore {
+    pub fn new() -> Self {
+        let (account, block_header, block_chain, notes) = mock_inputs();
+        Self {
+            account,
+            block_header,
+            block_chain,
+            notes,
+        }
+    }
+}
+
+impl Default for MockDataStore {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
+impl DataStore for MockDataStore {
+    fn get_transaction_data(
+        &self,
+        account_id: AccountId,
+        block_num: u32,
+        notes: &[NoteOrigin],
+    ) -> Result<(Account, BlockHeader, ChainMmr, Vec<Note>), DataStoreError> {
+        assert_eq!(account_id, self.account.id());
+        assert_eq!(block_num as u64, self.block_header.block_num().as_int());
+        assert_eq!(notes.len(), self.notes.len());
+        let origins = self
+            .notes
+            .iter()
+            .map(|note| note.proof().as_ref().unwrap().origin())
+            .collect::<Vec<_>>();
+        notes.iter().all(|note| origins.contains(&note));
+        Ok((
+            self.account.clone(),
+            self.block_header.clone(),
+            self.block_chain.clone(),
+            self.notes.clone(),
+        ))
+    }
+
+    fn get_account_code(
+        &self,
+        account_id: AccountId,
+    ) -> Result<assembly::ast::ModuleAst, DataStoreError> {
+        assert_eq!(account_id, self.account.id());
+        Ok(self.account.code().module().clone())
+    }
+}
