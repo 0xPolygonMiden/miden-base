b775186d09fe0feb36bab1cdf170f8cc2eeb04e0
frisitano <francesco.risitano95@gmail.com>
2023-07-18 10:27:03 +0200

    update

--- miden-tx/src/mock.rs
+++ miden-tx/src/mock.rs
@@ -35,23 +35,24 @@ impl DataStore for MockDataStore {
         &self,
         account_id: AccountId,
         block_num: u32,
-        notes: &[NoteOrigin],
+        note_origins: &[NoteOrigin],
     ) -> Result<(Account, BlockHeader, ChainMmr, Vec<Note>), DataStoreError> {
         assert_eq!(account_id, self.account.id());
         assert_eq!(block_num as u64, self.block_header.block_num().as_int());
-        assert_eq!(notes.len(), self.notes.len());
-        let origins = self
-            .notes
+        let notes = note_origins
             .iter()
-            .map(|note| note.proof().as_ref().unwrap().origin())
-            .collect::<Vec<_>>();
-        notes.iter().all(|note| origins.contains(&note));
-        Ok((
-            self.account.clone(),
-            self.block_header.clone(),
-            self.block_chain.clone(),
-            self.notes.clone(),
-        ))
+            .map(|origin| {
+                self.notes
+                    .iter()
+                    .find(|x| x.proof().as_ref().unwrap().origin() == origin)
+                    .cloned()
+                    .ok_or(DataStoreError::NoteNotFound(
+                        origin.block_num.as_int() as u32,
+                        origin.node_index,
+                    ))
+            })
+            .collect::<Result<Vec<_>, DataStoreError>>()?;
+        Ok((self.account.clone(), self.block_header.clone(), self.block_chain.clone(), notes))
     }
 
     fn get_account_code(
