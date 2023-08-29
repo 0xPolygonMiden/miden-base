4a45745b14674918929f0691e568d3f600d903a4
Dominik1999 <schmid.dominik.muc@gmail.com>
2023-08-29 13:50:38 +0200

    WIP feat: adding a simple wallet

--- miden-lib/tests/test_miden_wallet.rs
+++ miden-lib/tests/test_miden_wallet.rs
@@ -1,38 +1,33 @@
+pub mod common;
+use common::{
+    data::{mock_inputs, AccountStatus},
+    prepare_transaction, run_tx, MemAdviceProvider,
+};
+use vm_core::StackInputs;
 
+#[test]
+// Doesn't work yet. WIP. It compiles though.
 fn test_add_asset_via_wallet() {
-    for storage_item in [STORAGE_ITEM_0, STORAGE_ITEM_1] {
-        let (account, block_header, chain, notes) = mock_inputs(AccountStatus::Existing);
+    let (account, block_header, chain, notes) = mock_inputs(AccountStatus::Existing);
 
-        let code = format!(
-            "
-        use.miden::sat::miden_wallet
+    let code = format!(
+        "
+    use.miden::sat::miden_wallet
 
-        begin
-            # prepare the transaction
-            exec.miden_wallet::add_asset
+    # add the asset
+    begin
+        exec.miden_wallet::receive_asset
+    end
+    "
+    );
 
-            # push the account storage item index
-            push.{item_index}
+    let transaction =
+        prepare_transaction(account, None, block_header, chain, notes, &code, "", None, None);
 
-            # get the item
-            exec.account::get_item
-
-            # assert the item value is correct
-            push.{item_value} assert_eqw
-        end
-        ",
-            item_index = storage_item.0,
-            item_value = prepare_word(&storage_item.1)
-        );
-
-        let transaction =
-            prepare_transaction(account, None, block_header, chain, notes, &code, "", None, None);
-
-        let _process = run_tx(
-            transaction.tx_program().clone(),
-            StackInputs::from(transaction.stack_inputs()),
-            MemAdviceProvider::from(transaction.advice_provider_inputs()),
-        )
-        .unwrap();
-    }
+    let _process = run_tx(
+        transaction.tx_program().clone(),
+        StackInputs::from(transaction.stack_inputs()),
+        MemAdviceProvider::from(transaction.advice_provider_inputs()),
+    )
+    .unwrap();
 }
