4a5a72717324e6fe5148defb6f3d5c8dc0d26a9a
Nacho <amigoigna@gmail.com>
2023-11-30 14:30:07 -0300

    Comment fix

--- objects/src/accounts/account_id.rs
+++ objects/src/accounts/account_id.rs
@@ -192,6 +192,7 @@ impl AccountId {
         Ok(())
     }
 
+    /// Creates an Account Id from a hex string. Assumes the string starts with "0x"
     pub fn from_hex(hex_value: &str) -> Result<AccountId, AccountError> {
         miden_crypto::utils::hex_to_bytes(hex_value)
             .map_err(|err| AccountError::HexParseError(err.to_string()))
@@ -203,6 +204,7 @@ impl AccountId {
             })
     }
 
+    /// Returns a big-endian, hex-encoded string
     pub fn to_hex(&self) -> String {
         format!("0x{:02x}", self.0.as_int())
     }
