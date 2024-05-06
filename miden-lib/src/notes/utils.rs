1358317d3292239a118bd17729963da009b16c70
Bobbin Threadbare <bobbinth@protonmail.com>
2024-05-06 10:02:04 -0700

    chore: rustfmt fixes

--- miden-lib/src/notes/utils.rs
+++ miden-lib/src/notes/utils.rs
@@ -18,7 +18,7 @@ pub fn build_note_script(bytes: &[u8]) -> Result<NoteScript, NoteError> {
 }
 
 /// Creates a [NoteRecipient] for the P2ID note.
-/// 
+///
 /// Notes created with this recipient will be P2ID notes consumable by the specified target
 /// account.
 pub fn build_p2id_recipient(
