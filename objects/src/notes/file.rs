b1f317c4e9f759b3f25567eb281709fd4910c2c2
tomyrd <chubyrd1@gmail.com>
2024-05-31 18:35:13 -0300

    Reorder variants

--- objects/src/notes/file.rs
+++ objects/src/notes/file.rs
@@ -8,12 +8,12 @@ use super::{Note, NoteDetails, NoteId, NoteInclusionProof};
 
 /// A serialized representation of a note.
 pub enum NoteFile {
+    /// The note's details aren't known.
+    NoteId(NoteId),
     /// The note has not yet been recorded on chain.
     NoteDetails(NoteDetails),
     /// The note has been recorded on chain.
     NoteWithProof(Note, NoteInclusionProof),
-    /// The note's details aren't known.
-    NoteId(NoteId),
 }
 
 // SERIALIZATION
@@ -23,18 +23,18 @@ impl Serializable for NoteFile {
     fn write_into<W: ByteWriter>(&self, target: &mut W) {
         target.write_bytes("note".as_bytes());
         match self {
-            NoteFile::NoteDetails(details) => {
+            NoteFile::NoteId(note_id) => {
                 target.write_u8(0);
+                note_id.write_into(target);
+            },
+            NoteFile::NoteDetails(details) => {
+                target.write_u8(1);
                 details.write_into(target);
             },
             NoteFile::NoteWithProof(note, proof) => {
-                target.write_u8(1);
+                target.write_u8(2);
                 note.write_into(target);
                 proof.write_into(target);
-            },
-            NoteFile::NoteId(note_id) => {
-                target.write_u8(2);
-                note_id.write_into(target);
             },
         }
     }
@@ -49,13 +49,13 @@ impl Deserializable for NoteFile {
             )));
         }
         match source.read_u8()? {
-            0 => Ok(NoteFile::NoteDetails(NoteDetails::read_from(source)?)),
-            1 => {
+            0 => Ok(NoteFile::NoteId(NoteId::read_from(source)?)),
+            1 => Ok(NoteFile::NoteDetails(NoteDetails::read_from(source)?)),
+            2 => {
                 let note = Note::read_from(source)?;
                 let proof = NoteInclusionProof::read_from(source)?;
                 Ok(NoteFile::NoteWithProof(note, proof))
             },
-            2 => Ok(NoteFile::NoteId(NoteId::read_from(source)?)),
             v => {
                 Err(DeserializationError::InvalidValue(format!("Unknown variant {v} for NoteFile")))
             },
