7c0e0889ffe1e5e482ad461c0b0874939d1ad803
tomyrd <chubyrd1@gmail.com>
2024-06-03 11:55:57 -0300

    Add serialization tests

--- objects/src/notes/file.rs
+++ objects/src/notes/file.rs
@@ -62,3 +62,122 @@ impl Deserializable for NoteFile {
         }
     }
 }
+
+// TESTS
+// ================================================================================================
+
+#[cfg(test)]
+mod tests {
+    use alloc::vec::Vec;
+
+    use assembly::{ast::ProgramAst, Assembler};
+    use vm_core::{
+        utils::{Deserializable, Serializable},
+        Felt,
+    };
+
+    use crate::{
+        accounts::{
+            account_id::testing::{
+                ACCOUNT_ID_FUNGIBLE_FAUCET_ON_CHAIN,
+                ACCOUNT_ID_REGULAR_ACCOUNT_UPDATABLE_CODE_OFF_CHAIN,
+            },
+            AccountId,
+        },
+        assets::{Asset, FungibleAsset},
+        notes::{
+            Note, NoteAssets, NoteFile, NoteInclusionProof, NoteInputs, NoteMetadata,
+            NoteRecipient, NoteScript, NoteTag, NoteType,
+        },
+    };
+
+    fn create_example_note() -> Note {
+        let faucet = AccountId::new_unchecked(Felt::new(ACCOUNT_ID_FUNGIBLE_FAUCET_ON_CHAIN));
+        let target = AccountId::new_unchecked(Felt::new(
+            ACCOUNT_ID_REGULAR_ACCOUNT_UPDATABLE_CODE_OFF_CHAIN,
+        ));
+
+        let serial_num = [Felt::new(0), Felt::new(1), Felt::new(2), Felt::new(3)];
+        let note_program_ast = ProgramAst::parse("begin push.1 drop end").unwrap();
+        let (script, _) = NoteScript::new(note_program_ast, &Assembler::default()).unwrap();
+        let note_inputs = NoteInputs::new(vec![target.into()]).unwrap();
+        let recipient = NoteRecipient::new(serial_num, script, note_inputs);
+
+        let asset = Asset::Fungible(FungibleAsset::new(faucet, 100).unwrap());
+        let metadata =
+            NoteMetadata::new(faucet, NoteType::Public, NoteTag::from(123), Felt::new(0)).unwrap();
+
+        Note::new(NoteAssets::new(vec![asset]).unwrap(), metadata, recipient)
+    }
+
+    #[test]
+    fn serialized_note_magic() {
+        let note = create_example_note();
+        let file = NoteFile::NoteId(note.id());
+        let mut buffer = Vec::new();
+        file.write_into(&mut buffer);
+
+        let magic_value = &buffer[..4];
+        assert_eq!(magic_value, b"note");
+    }
+
+    #[test]
+    fn serialize_id() {
+        let note = create_example_note();
+        let file = NoteFile::NoteId(note.id());
+        let mut buffer = Vec::new();
+        file.write_into(&mut buffer);
+
+        let file_copy = NoteFile::read_from_bytes(&buffer).unwrap();
+
+        match file_copy {
+            NoteFile::NoteId(note_id) => {
+                assert_eq!(note.id(), note_id);
+            },
+            _ => panic!("Invalid note file variant"),
+        }
+    }
+
+    #[test]
+    fn serialize_details() {
+        let note = create_example_note();
+        let file = NoteFile::NoteDetails(note.details.clone());
+        let mut buffer = Vec::new();
+        file.write_into(&mut buffer);
+
+        let file_copy = NoteFile::read_from_bytes(&buffer).unwrap();
+
+        match file_copy {
+            NoteFile::NoteDetails(details) => {
+                assert_eq!(details, note.details);
+            },
+            _ => panic!("Invalid note file variant"),
+        }
+    }
+
+    #[test]
+    fn serialize_with_proof() {
+        let note = create_example_note();
+        let mock_inclusion_proof = NoteInclusionProof::new(
+            Default::default(),
+            Default::default(),
+            Default::default(),
+            0,
+            Default::default(),
+        )
+        .unwrap();
+        let file = NoteFile::NoteWithProof(note.clone(), mock_inclusion_proof.clone());
+        let mut buffer = Vec::new();
+        file.write_into(&mut buffer);
+
+        let file_copy = NoteFile::read_from_bytes(&buffer).unwrap();
+
+        match file_copy {
+            NoteFile::NoteWithProof(note_copy, inclusion_proof_copy) => {
+                assert_eq!(note, note_copy);
+                assert_eq!(inclusion_proof_copy, mock_inclusion_proof);
+            },
+            _ => panic!("Invalid note file variant"),
+        }
+    }
+}
