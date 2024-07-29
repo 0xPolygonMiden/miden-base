a942c54bab7bfa1c08d89cb58fe5dd4c6afe05b0
omahs <73983677+omahs@users.noreply.github.com>
2024-07-30 01:58:20 +0200

    docs: fix typos (#805)

--- objects/src/transaction/prepared_tx.rs
+++ objects/src/transaction/prepared_tx.rs
@@ -8,7 +8,7 @@ use super::{
 /// A struct that contains all of the data required to execute a transaction.
 ///
 /// This includes:
-/// - A an executable program which defines the transaction.
+/// - An executable program which defines the transaction.
 /// - An optional transaction script.
 /// - A set of inputs against which the transaction program should be executed.
 #[derive(Debug)]
