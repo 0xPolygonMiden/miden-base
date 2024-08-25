d19745d7efd5a65d9940c22300eb033f78efe9fe
Ignacio Amigo <ignacio.amigo@lambdaclass.com>
2024-08-25 19:22:43 -0300

    Call method correctly

--- miden-tx/src/testing/mock_chain.rs
+++ miden-tx/src/testing/mock_chain.rs
@@ -369,8 +369,10 @@ impl MockChain {
     ) -> Account {
         let (account, seed, authenticator) = match auth_method {
             Auth::BasicAuth => {
+                let mut rng = StdRng::from_entropy();
+
                 let (acc, seed, auth) =
-                    account_builder.build_with_auth(&TransactionKernel::assembler()).unwrap();
+                    account_builder.build_with_auth(&TransactionKernel::assembler(), &mut rng).unwrap();
 
                 let authenticator = BasicAuthenticator::<StdRng>::new(&[(
                     auth.public_key().into(),
