eaa0465e22bec68c5c4a86cb384c555ff4790022
Andrey <andrey@polygon.technology>
2024-09-18 21:28:13 +0300

    refactor: update names of constants, move storage slot info to even pointer

--- miden-lib/src/transaction/memory.rs
+++ miden-lib/src/transaction/memory.rs
@@ -230,7 +230,7 @@ pub const NATIVE_ACCT_PROCEDURES_SECTION_PTR: MemoryAddress =
 
 /// The offset at which the number of storage slots contained in the account storage is stored
 /// relative to the start of the account data segment.
-pub const NUM_ACCT_STORAGE_SLOTS_OFFSET: MemoryAddress = 520;
+pub const NUM_ACCT_STORAGE_SLOTS_OFFSET: MemoryAddress = 521;
 
 /// The memory address at which number of storage slots contained in the account storage is stored
 /// in the native account.
@@ -239,7 +239,7 @@ pub const NATIVE_NUM_ACCT_STORAGE_SLOTS_PTR: MemoryAddress =
 
 /// The offset at which the account storage slots section begins relative to the start of the
 /// account data segment.
-pub const ACCT_STORAGE_SLOTS_SECTION_OFFSET: MemoryAddress = 521;
+pub const ACCT_STORAGE_SLOTS_SECTION_OFFSET: MemoryAddress = 522;
 
 /// The memory address at which the account storage slots section begins in the native account.
 pub const NATIVE_ACCT_STORAGE_SLOTS_SECTION_PTR: MemoryAddress =
