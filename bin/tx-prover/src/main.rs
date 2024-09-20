c1226558fab40ba74417dc7af88e9bfb8b0b2ced
SantiagoPittella <pittellasantiago@gmail.com>
2024-09-20 17:37:04 -0300

    remove unwraps from main

--- bin/tx-prover/src/main.rs
+++ bin/tx-prover/src/main.rs
@@ -8,7 +8,7 @@ use tracing::info;
 pub mod server;
 
 #[tokio::main]
-async fn main() {
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
     // Initialize tracing subscriber with default settings for console output
     tracing_subscriber::fmt::init();
 
@@ -16,17 +16,18 @@ async fn main() {
     let port = env::var("PROVER_SERVICE_PORT").unwrap_or_else(|_| "50051".to_string());
     let addr = format!("{}:{}", host, port);
 
-    let rpc = Rpc::new(TcpListener::bind(addr).await.unwrap());
+    let rpc = Rpc::new(TcpListener::bind(addr).await?);
 
-    info!("Server listening on {}", rpc.listener.local_addr().unwrap());
+    info!("Server listening on {}", rpc.listener.local_addr()?);
 
     // build our application with a route
     tonic::transport::Server::builder()
         .accept_http1(true)
         .add_service(tonic_web::enable(rpc.api_service))
         .serve_with_incoming(TcpListenerStream::new(rpc.listener))
-        .await
-        .unwrap();
+        .await?;
+
+    Ok(())
 }
 
 #[cfg(test)]
