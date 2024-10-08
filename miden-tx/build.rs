79633ca9f119ff190cd0a29f89ec725e4afb0a70
Philipp Gackstatter <PhilippGackstatter@users.noreply.github.com>
2024-10-08 14:42:53 +0200

    feat(tx): Check for duplicate error codes

--- miden-tx/build.rs
+++ miden-tx/build.rs
@@ -56,7 +56,7 @@ fn main() -> Result<()> {
 fn generate_kernel_error_constants(kernel_source_dir: &Path) -> Result<()> {
     // We use a BTree here to order the errors by their categories which is the first part after the
     // ERR_ prefix and to allow for the same error code to be defined multiple times in
-    // different files.
+    // different files (as long as the constant names match).
     let mut errors = BTreeMap::new();
 
     // Walk all files of the kernel source directory.
@@ -69,6 +69,16 @@ fn generate_kernel_error_constants(kernel_source_dir: &Path) -> Result<()> {
         extract_kernel_errors(&mut errors, &file_contents)?;
     }
 
+    // Check if any error code is used twice with differnt error names.
+    let mut error_codes = BTreeMap::new();
+    for (error_name, error) in errors.iter() {
+        if let Some(existing_error_name) = error_codes.get(&error.code) {
+            return Err(Report::msg(format!("Transaction kernel error code 0x{} is used multiple times; Non-exhaustive list: ERR_{existing_error_name}, ERR_{error_name}", error.code)));
+        }
+
+        error_codes.insert(error.code.clone(), error_name);
+    }
+
     // Generate the errors file.
     let error_file_content = generate_kernel_errors(errors)?;
     std::fs::write(KERNEL_ERRORS_FILE, error_file_content).into_diagnostic()?;
@@ -77,7 +87,7 @@ fn generate_kernel_error_constants(kernel_source_dir: &Path) -> Result<()> {
 }
 
 fn extract_kernel_errors(
-    errors: &mut BTreeMap<String, (String, String)>,
+    errors: &mut BTreeMap<ErrorName, ExtractedError>,
     file_contents: &str,
 ) -> Result<()> {
     let regex =
@@ -85,10 +95,18 @@ fn extract_kernel_errors(
             .unwrap();
 
     for capture in regex.captures_iter(file_contents) {
-        let error_name =
-            capture.name("name").expect("error name should be captured").as_str().to_owned();
-        let error_code =
-            capture.name("code").expect("error code should be captured").as_str().to_owned();
+        let error_name = capture
+            .name("name")
+            .expect("error name should be captured")
+            .as_str()
+            .trim()
+            .to_owned();
+        let error_code = capture
+            .name("code")
+            .expect("error code should be captured")
+            .as_str()
+            .trim()
+            .to_owned();
 
         let error_message = match capture.name("message") {
             Some(message) => message.as_str().trim().to_owned(),
@@ -97,13 +115,13 @@ fn extract_kernel_errors(
             },
         };
 
-        if let Some((existing_error_code, _)) = errors.get(&error_name) {
+        if let Some(ExtractedError { code: existing_error_code, .. }) = errors.get(&error_name) {
             if existing_error_code != &error_code {
                 return Err(Report::msg(format!("Transaction kernel error constant ERR_{error_name} is already defined elsewhere but its error code is different")));
             }
         }
 
-        errors.insert(error_name, (error_code, error_message));
+        errors.insert(error_name, ExtractedError { code: error_code, message: error_message });
     }
 
     Ok(())
@@ -126,7 +144,7 @@ fn is_new_error_category<'a>(last_error: &mut Option<&'a str>, current_error: &'
     is_new
 }
 
-fn generate_kernel_errors(errors: BTreeMap<String, (String, String)>) -> Result<String> {
+fn generate_kernel_errors(errors: BTreeMap<ErrorName, ExtractedError>) -> Result<String> {
     let mut output = String::new();
 
     writeln!(
@@ -146,12 +164,12 @@ fn generate_kernel_errors(errors: BTreeMap<String, (String, String)>) -> Result<
     .into_diagnostic()?;
 
     let mut last_error = None;
-    for (error_name, (error_code, _)) in errors.iter() {
+    for (error_name, ExtractedError { code, .. }) in errors.iter() {
         // Group errors into blocks separate by newlines.
         if is_new_error_category(&mut last_error, error_name) {
             writeln!(output).into_diagnostic()?;
         }
-        writeln!(output, "pub const ERR_{error_name}: u32 = 0x{error_code};").into_diagnostic()?;
+        writeln!(output, "pub const ERR_{error_name}: u32 = 0x{code};").into_diagnostic()?;
     }
     writeln!(output).into_diagnostic()?;
 
@@ -159,15 +177,22 @@ fn generate_kernel_errors(errors: BTreeMap<String, (String, String)>) -> Result<
         .into_diagnostic()?;
 
     let mut last_error = None;
-    for (error_name, (_, error_message)) in errors.iter() {
+    for (error_name, ExtractedError { message, .. }) in errors.iter() {
         // Group errors into blocks separate by newlines.
         if is_new_error_category(&mut last_error, error_name) {
             writeln!(output).into_diagnostic()?;
         }
-        writeln!(output, r#"    (ERR_{error_name}, "{error_message}"),"#).into_diagnostic()?;
+        writeln!(output, r#"    (ERR_{error_name}, "{message}"),"#).into_diagnostic()?;
     }
 
     writeln!(output, "];").into_diagnostic()?;
 
     Ok(output)
+}
+
+type ErrorName = String;
+
+struct ExtractedError {
+    code: String,
+    message: String,
 }
