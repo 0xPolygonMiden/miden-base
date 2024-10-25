b50ddd81debd6a129c1ca3beacfedaaa4796e143
SantiagoPittella <pittellasantiago@gmail.com>
2024-10-25 12:00:02 -0300

    feat(prover-proxy): rate limiting, health check

--- bin/prover-proxy/src/main.rs
+++ bin/prover-proxy/src/main.rs
@@ -1,41 +1,75 @@
 use async_trait::async_trait;
+use once_cell::sync::Lazy;
+use pingora::http::ResponseHeader;
 use pingora::prelude::*;
+use pingora::upstreams::peer::ALPN;
+use pingora_core::server::Server;
+use pingora_core::upstreams::peer::HttpPeer;
+use pingora_core::Result;
+use pingora_core::{modules::http::HttpModules, prelude::Opt};
+use pingora_limits::rate::Rate;
+use pingora_proxy::{ProxyHttp, Session};
+
 use std::sync::Arc;
+use std::time::Duration;
 
 fn main() {
-    let mut my_server = Server::new(None).unwrap();
-    my_server.bootstrap();
+    let mut server = Server::new(Some(Opt::default())).unwrap();
+    server.bootstrap();
+    let mut upstreams = LoadBalancer::try_from_iter(["0.0.0.0:8080", "0.0.0.0:50051"]).unwrap();
 
-    let upstreams = LoadBalancer::try_from_iter(["127.0.0.1:8081", "127.0.0.1:8082"]).unwrap();
+    // Set health check
+    // let hc = TcpHealthCheck::new();
+    // upstreams.set_health_check(hc);
+    // upstreams.health_check_frequency = Some(Duration::from_secs(1));
+    // Set background service
+    // let background = background_service("health check", upstreams);
+    // let upstreams = background.task();
 
-    let mut lb = http_proxy_service(&my_server.configuration, LB(Arc::new(upstreams)));
+    // Set load balancer
+    let mut lb = http_proxy_service(&server.configuration, LB(upstreams.into()));
     lb.add_tcp("0.0.0.0:6188");
 
-    my_server.add_service(lb);
-
-    my_server.run_forever();
+    // let rate = Rate
+    // server.add_service(background);
+    server.add_service(lb);
+    server.run_forever();
 }
 
 pub struct LB(Arc<LoadBalancer<RoundRobin>>);
 
+impl LB {
+    pub fn get_request_appid(&self, session: &mut Session) -> Option<String> {
+        match session.req_header().headers.get("appid").map(|v| v.to_str()) {
+            None => None,
+            Some(v) => match v {
+                Ok(v) => Some(v.to_string()),
+                Err(_) => None,
+            },
+        }
+    }
+}
+
+// Rate limiter
+static RATE_LIMITER: Lazy<Rate> = Lazy::new(|| Rate::new(Duration::from_secs(1)));
+
+// max request per second per client
+static MAX_REQ_PER_SEC: isize = 1;
+
 #[async_trait]
 impl ProxyHttp for LB {
-    /// For this small example, we don't need context storage
     type CTX = ();
-    fn new_ctx(&self) -> () {
-        ()
-    }
 
-    async fn upstream_peer(&self, _session: &mut Session, _ctx: &mut ()) -> Result<Box<HttpPeer>> {
-        let upstream = self
-            .0
-            .select(b"", 256) // hash doesn't matter for round robin
-            .unwrap();
+    fn new_ctx(&self) {}
 
-        println!("upstream peer is: {upstream:?}");
-
-        // Set SNI to one.one.one.one
-        let peer = Box::new(HttpPeer::new(upstream, true, "one.one.one.one".to_string()));
+    async fn upstream_peer(
+        &self,
+        _session: &mut Session,
+        _ctx: &mut Self::CTX,
+    ) -> Result<Box<HttpPeer>> {
+        let upstream = self.0.select(b"", 256).unwrap();
+        // Set SNI
+        let mut peer = Box::new(HttpPeer::new(upstream, false, "".to_string()));
         Ok(peer)
     }
 
@@ -44,8 +78,43 @@ impl ProxyHttp for LB {
         _session: &mut Session,
         upstream_request: &mut RequestHeader,
         _ctx: &mut Self::CTX,
-    ) -> Result<()> {
-        upstream_request.insert_header("Host", "one.one.one.one").unwrap();
+    ) -> Result<()>
+    where
+        Self::CTX: Send + Sync,
+    {
+        // Check if it's a gRPC request
+        if let Some(content_type) = upstream_request.headers.get("content-type") {
+            if content_type == "application/grpc" {
+                // Ensure the correct host and gRPC headers are forwarded
+                // upstream_request.insert_header("Host", "0.0.0.0").unwrap();
+                upstream_request.insert_header("content-type", "application/grpc").unwrap();
+            }
+        }
+
         Ok(())
+    }
+
+    async fn request_filter(&self, session: &mut Session, _ctx: &mut Self::CTX) -> Result<bool>
+    where
+        Self::CTX: Send + Sync,
+    {
+        let appid = match self.get_request_appid(session) {
+            None => return Ok(false), // no client appid found, skip rate limiting
+            Some(addr) => addr,
+        };
+
+        // retrieve the current window requests
+        let curr_window_requests = RATE_LIMITER.observe(&appid, 1);
+        if curr_window_requests > MAX_REQ_PER_SEC {
+            // rate limited, return 429
+            let mut header = ResponseHeader::build(429, None).unwrap();
+            header.insert_header("X-Rate-Limit-Limit", MAX_REQ_PER_SEC.to_string()).unwrap();
+            header.insert_header("X-Rate-Limit-Remaining", "0").unwrap();
+            header.insert_header("X-Rate-Limit-Reset", "1").unwrap();
+            session.set_keepalive(None);
+            session.write_response_header(Box::new(header), true).await?;
+            return Ok(true);
+        }
+        Ok(false)
     }
 }
