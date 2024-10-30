25ac06f9710fa71b3714d17fcfa0af26810aa5a0
Andrey <andrey@polygon.technology>
2024-10-30 21:35:12 +0300

    refactor: roll back the fpi test

--- miden-tx/src/tests/kernel_tests/test_tx.rs
+++ miden-tx/src/tests/kernel_tests/test_tx.rs
@@ -1,8 +1,5 @@
 use alloc::vec::Vec;
-use std::{
-    string::{String, ToString},
-    sync::Arc,
-};
+use std::string::String;
 
 use miden_lib::transaction::{
     memory::{
@@ -32,7 +29,7 @@ use miden_objects::{
         account::AccountBuilder, constants::NON_FUNGIBLE_ASSET_DATA_2, prepare_word,
         storage::STORAGE_LEAVES_2,
     },
-    transaction::{OutputNote, OutputNotes, TransactionScript},
+    transaction::{OutputNote, OutputNotes},
     Digest, FieldElement,
 };
 use rand::SeedableRng;
@@ -49,8 +46,7 @@ use crate::{
         mock_chain::{MockChain, MockChainBuilder},
         MockHost, TransactionContextBuilder,
     },
-    tests::kernel_tests::read_root_mem_value,
-    TransactionExecutor,
+    tests::kernel_tests::{read_root_mem_value, try_read_root_mem_value},
 };
 
 #[test]
@@ -761,48 +757,34 @@ fn test_load_foreign_account_basic() {
 /// the loaded account.
 #[test]
 fn test_load_foreign_account_twice() {
-    let foreign_account_code_source = "
-        use.miden::account
-
-        export.get_item_foreign
-            # make this foreign procedure unique to make sure that we invoke the procedure of the 
-            # foreign account, not the native one
-            push.1 drop
-            exec.account::get_item_foreign
-        end
-
-        export.get_map_item_foreign
-            # make this foreign procedure unique to make sure that we invoke the procedure of the 
-            # foreign account, not the native one
-            push.2 drop
-            exec.account::get_map_item_foreign
-        end
-    ";
-    let foreign_account_code = AccountCode::mock_with_code(
-        foreign_account_code_source,
-        TransactionKernel::testing_assembler(),
-    );
-
     let storage_slot = AccountStorage::mock_item_0().slot;
     let (foreign_account, _) = AccountBuilder::new(ChaCha20Rng::from_entropy())
         .add_storage_slot(storage_slot)
-        .code(foreign_account_code.clone())
+        .code(AccountCode::mock_account_code(TransactionKernel::testing_assembler(), false))
         .nonce(ONE)
         .build()
         .unwrap();
 
-    let foreign_account_id = foreign_account.id();
+    let account_id = foreign_account.id();
     let mock_chain = MockChainBuilder::default().accounts(vec![foreign_account.clone()]).build();
     let advice_inputs = get_mock_advice_inputs(&foreign_account, &mock_chain);
+
+    let tx_context = TransactionContextBuilder::with_standard_account(ONE)
+        .mock_chain(mock_chain)
+        .advice_inputs(advice_inputs.clone())
+        .build();
 
     let code = format!(
         "
         use.std::sys
 
+        use.kernel::prologue
         use.miden::tx
         use.miden::account
 
         begin
+            exec.prologue::prepare_transaction
+
             ### Get the storage item at index 0 #####################
             # pad the stack for the `execute_foreign_procedure`execution
             padw padw push.0.0.0
@@ -812,10 +794,10 @@ fn test_load_foreign_account_twice() {
             push.0
 
             # get the hash of the `get_item_foreign` account procedure
-            push.{get_item_foreign_hash}
+            procref.account::get_item_foreign
 
             # push the foreign account id
-            push.{foreign_account_id}
+            push.{account_id}
             # => [foreign_account_id, FOREIGN_PROC_ROOT, storage_item_index, pad(11)]
 
             exec.tx::execute_foreign_procedure dropw
@@ -823,58 +805,34 @@ fn test_load_foreign_account_twice() {
 
             ### Get the storage item at index 0 again ###############
             # pad the stack for the `execute_foreign_procedure`execution
-            padw push.0.0.0
-            # => [pad(7)]
+            padw padw push.0.0.0
+            # => [pad(11)]
 
             # push the index of desired storage item
             push.0
 
             # get the hash of the `get_item_foreign` account procedure
-            push.{get_item_foreign_hash}
+            procref.account::get_item_foreign
 
             # push the foreign account id
-            push.{foreign_account_id}
-            # => [foreign_account_id, FOREIGN_PROC_ROOT, storage_item_index, MAP_ITEM_KEY, pad(7)]
+            push.{account_id}
+            # => [foreign_account_id, FOREIGN_PROC_ROOT, storage_item_index, MAP_ITEM_KEY, pad(11)]
 
             exec.tx::execute_foreign_procedure
-
-            # assert the correctness of the obtained value
-            push.1.2.3.4 assert_eqw
 
             # truncate the stack
             exec.sys::truncate_stack
         end
         ",
-        // `get_item_foreign` procedure has index 0
-        get_item_foreign_hash = foreign_account_code.procedures()[0].mast_root()
     );
 
-    let tx_script =
-        TransactionScript::compile(code, vec![], TransactionKernel::testing_assembler()).unwrap();
+    let process = tx_context.execute_code(&code).unwrap();
 
-    let tx_context = TransactionContextBuilder::with_standard_account(ONE)
-        .mock_chain(mock_chain)
-        .advice_inputs(advice_inputs.clone())
-        .tx_script(tx_script)
-        .build();
-
-    let block_ref = tx_context.tx_inputs().block_header().block_num();
-    let note_ids = tx_context
-        .tx_inputs()
-        .input_notes()
-        .iter()
-        .map(|note| note.id())
-        .collect::<Vec<_>>();
-    let account = tx_context.account();
-
-    let mut executor: TransactionExecutor =
-        TransactionExecutor::new(Arc::new(tx_context.clone()), None).with_tracing();
-    executor.load_account_code(foreign_account.code());
-
-    let _executed_transaction = executor
-        .execute_transaction(account.id(), block_ref, &note_ids, tx_context.tx_args().clone())
-        .map_err(|e| e.to_string())
-        .unwrap();
+    assert_eq!(
+        try_read_root_mem_value(&process, NATIVE_ACCOUNT_DATA_PTR + ACCOUNT_DATA_LENGTH as u32 * 2),
+        None,
+        "Memory starting from 6144 should stay uninitialized"
+    );
 }
 
 // HELPER FUNCTIONS
