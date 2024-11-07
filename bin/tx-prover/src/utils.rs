77fdaa2e4fd1c9682e5af0b7ead404230f86abf1
SantiagoPittella <pittellasantiago@gmail.com>
2024-11-07 13:48:24 -0300

    update comment in util's function

--- bin/tx-prover/src/utils.rs
+++ bin/tx-prover/src/utils.rs
@@ -8,8 +8,8 @@ use crate::commands::CliConfig;
 
 /// Loads config file from current directory and default filename and returns it
 ///
-/// This function will look for the configuration file at the provided path. If the path is
-/// relative, searches in parent directories all the way to the root as well.
+/// This function will look for the configuration file with the name defined at the
+/// [PROVER_SERVICE_CONFIG_FILE_NAME] constant in the current directory.
 pub(crate) fn load_config_from_file() -> Result<CliConfig, String> {
     let mut current_dir = std::env::current_dir().map_err(|err| err.to_string())?;
     current_dir.push(PROVER_SERVICE_CONFIG_FILE_NAME);
