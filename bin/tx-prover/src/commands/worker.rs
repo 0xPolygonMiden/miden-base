f5a917246168edc5e9dff417ef8c5ce60a693bc0
SantiagoPittella <pittellasantiago@gmail.com>
2024-11-07 13:48:48 -0300

    use cli options for workers config

--- bin/tx-prover/src/commands/worker.rs
+++ bin/tx-prover/src/commands/worker.rs
@@ -1,13 +1,20 @@
 use clap::Parser;
-use tokio::{net::TcpListener, task};
+use tokio::net::TcpListener;
 use tokio_stream::wrappers::TcpListenerStream;
 use tracing::info;
 
-use crate::{api::RpcListener, utils::load_config_from_file};
+use crate::api::RpcListener;
 
 /// Starts the workers defined in the config file.
 #[derive(Debug, Parser)]
-pub struct StartWorker;
+pub struct StartWorker {
+    /// The host of the worker
+    #[clap(short, long, default_value = "0.0.0.0")]
+    host: String,
+    /// The port of the worker
+    #[clap(short, long, default_value = "50051")]
+    port: u16,
+}
 
 impl StartWorker {
     /// Starts the workers defined in the config file.
@@ -16,41 +23,21 @@ impl StartWorker {
     /// then start a server for each worker and wait for all servers to complete.
     pub async fn execute(&self) -> Result<(), String> {
         tracing_subscriber::fmt::init();
-        let cli_config = load_config_from_file()?;
-        let workers_addrs = cli_config
-            .workers
-            .iter()
-            .map(|worker| format!("{}:{}", worker.host, worker.port))
-            .collect::<Vec<String>>();
+        let worker_addr = format!("{}:{}", self.host, self.port);
+        let rpc =
+            RpcListener::new(TcpListener::bind(&worker_addr).await.map_err(|err| err.to_string())?);
 
-        let mut handles = Vec::new();
+        info!(
+            "Server listening on {}",
+            rpc.listener.local_addr().map_err(|err| err.to_string())?
+        );
 
-        for worker_addr in workers_addrs {
-            let handle = task::spawn(async move {
-                let rpc = RpcListener::new(
-                    TcpListener::bind(&worker_addr).await.map_err(|err| err.to_string())?,
-                );
-
-                info!(
-                    "Server listening on {}",
-                    rpc.listener.local_addr().map_err(|err| err.to_string())?
-                );
-
-                tonic::transport::Server::builder()
-                    .accept_http1(true)
-                    .add_service(tonic_web::enable(rpc.api_service))
-                    .serve_with_incoming(TcpListenerStream::new(rpc.listener))
-                    .await
-                    .map_err(|err| err.to_string())
-            });
-
-            handles.push(handle);
-        }
-
-        // Wait for all server tasks to complete
-        for handle in handles {
-            handle.await.map_err(|err| err.to_string())??;
-        }
+        tonic::transport::Server::builder()
+            .accept_http1(true)
+            .add_service(tonic_web::enable(rpc.api_service))
+            .serve_with_incoming(TcpListenerStream::new(rpc.listener))
+            .await
+            .map_err(|err| err.to_string())?;
 
         Ok(())
     }
