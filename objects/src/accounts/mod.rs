8bb0878b9b7c82c4dea11c6224ce4e5b3f244cfd
Philipp Gackstatter <PhilippGackstatter@users.noreply.github.com>
2024-11-13 10:27:21 +0100

    feat: Make `initialize_from_components` private

--- objects/src/accounts/mod.rs
+++ objects/src/accounts/mod.rs
@@ -140,7 +140,7 @@ impl Account {
     /// - Two or more libraries export a procedure with the same MAST root.
     /// - The number of [`StorageSlot`]s of all components exceeds 255.
     /// - [`MastForest::merge`](vm_processor::MastForest::merge) fails on all libraries.
-    pub fn initialize_from_components(
+    pub(super) fn initialize_from_components(
         account_type: AccountType,
         components: &[AccountComponent],
     ) -> Result<(AccountCode, AccountStorage), AccountError> {
