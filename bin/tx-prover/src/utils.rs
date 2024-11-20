50aa3f3e4b5c005294ddbff5f2ab8377c7bc5911
SantiagoPittella <pittellasantiago@gmail.com>
2024-11-20 17:24:53 -0300

    feat: use struct for queue, add methods for context, move functions to utils

--- bin/tx-prover/src/utils.rs
+++ bin/tx-prover/src/utils.rs
@@ -3,8 +3,12 @@ use figment::{
     Figment,
 };
 use miden_tx_prover::PROVER_SERVICE_CONFIG_FILE_NAME;
+use pingora::{http::ResponseHeader, Error, ErrorType};
+use pingora_proxy::Session;
 
 use crate::commands::ProxyConfig;
+
+const RESOURCE_EXHAUSTED_CODE: u16 = 8;
 
 /// Loads config file from current directory and default filename and returns it
 ///
@@ -26,4 +30,40 @@ pub(crate) fn setup_tracing() {
         std::env::set_var("RUST_LOG", "info"); // Default to 'info' level
     }
     tracing_subscriber::fmt::init();
+}
+
+/// Create a 503 response for a full queue
+pub(crate) async fn create_queue_full_response(
+    session: &mut Session,
+) -> pingora_core::Result<bool> {
+    // Set grpc-message header to "Too many requests in the queue"
+    // This is meant to be used by a Tonic interceptor to return a gRPC error
+    let mut header = ResponseHeader::build(503, None)?;
+    header.insert_header("grpc-message", "Too many requests in the queue".to_string())?;
+    header.insert_header("grpc-status", RESOURCE_EXHAUSTED_CODE)?;
+    session.set_keepalive(None);
+    session.write_response_header(Box::new(header.clone()), true).await?;
+
+    let mut error = Error::new(ErrorType::HTTPStatus(503))
+        .more_context("Too many requests in the queue")
+        .into_in();
+    error.set_cause("Too many requests in the queue");
+
+    session.write_response_header(Box::new(header), false).await?;
+    Err(error)
+}
+
+/// Create a 429 response for too many requests
+pub async fn create_too_many_requests_response(
+    session: &mut Session,
+    max_request_per_second: isize,
+) -> pingora_core::Result<bool> {
+    // Rate limited, return 429
+    let mut header = ResponseHeader::build(429, None)?;
+    header.insert_header("X-Rate-Limit-Limit", max_request_per_second.to_string())?;
+    header.insert_header("X-Rate-Limit-Remaining", "0")?;
+    header.insert_header("X-Rate-Limit-Reset", "1")?;
+    session.set_keepalive(None);
+    session.write_response_header(Box::new(header), true).await?;
+    Ok(true)
 }
