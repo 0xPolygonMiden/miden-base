e32b13482573360f862857d8ed3a9e3ca7a7e15b
Philipp Gackstatter <PhilippGackstatter@users.noreply.github.com>
2025-01-24 13:20:42 +0100

    feat: Add `ProposedBatch`

--- crates/miden-batch-prover/src/lib.rs
+++ crates/miden-batch-prover/src/lib.rs
@@ -5,5 +5,8 @@ extern crate alloc;
 #[cfg(feature = "std")]
 extern crate std;
 
+mod proposed_batch;
+pub use proposed_batch::ProposedBatch;
+
 #[cfg(any(feature = "testing", test))]
 pub mod testing;
