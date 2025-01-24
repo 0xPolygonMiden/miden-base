2b44b453dc0fcbb8c11498a34fac255c4152e7c6
Philipp Gackstatter <PhilippGackstatter@users.noreply.github.com>
2025-01-24 10:22:37 +0100

    feat: Add `BatchId`

---
+++ crates/miden-objects/src/batch/batch_id.rs
@@ -0,0 +1,38 @@
+use alloc::vec::Vec;
+use core::borrow::Borrow;
+
+use miden_crypto::hash::Digest;
+use vm_core::crypto::hash::Blake3Digest;
+use vm_processor::crypto::Blake3_256;
+
+use crate::transaction::TransactionId;
+
+// BATCH ID
+// ================================================================================================
+
+/// Uniquely identifies a [`TransactionBatch`].
+// TODO: Should this really be a Blake3 hash? We have to compute this in the block kernel
+// eventually, so we'd probably want RPO instead?
+// TODO: Compute batch ID as hash over tx ID _and_ account ID.
+#[derive(Debug, Copy, Clone, Eq, Ord, PartialEq, PartialOrd)]
+pub struct BatchId(Blake3Digest<32>);
+
+impl BatchId {
+    /// Calculates a batch ID from the given set of transactions.
+    pub fn compute<T>(txs: impl Iterator<Item = T>) -> Self
+    where
+        T: Borrow<TransactionId>,
+    {
+        let mut buf = Vec::with_capacity(32 * txs.size_hint().0);
+        for tx in txs {
+            buf.extend_from_slice(&tx.borrow().as_bytes());
+        }
+        Self(Blake3_256::hash(&buf))
+    }
+}
+
+impl std::fmt::Display for BatchId {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.write_str(&miden_crypto::utils::bytes_to_hex_string(self.0.as_bytes()))
+    }
+}
--- crates/miden-objects/src/batch/mod.rs
+++ crates/miden-objects/src/batch/mod.rs
@@ -1,2 +1,5 @@
 mod note_tree;
 pub use note_tree::BatchNoteTree;
+
+mod batch_id;
+pub use batch_id::BatchId;
