c3b3fe2cfbad1679c02da4843547aa3397dbf760
Philipp Gackstatter <PhilippGackstatter@users.noreply.github.com>
2025-01-27 15:35:10 +0100

    feat: Add `BatchAccountUpdate`

--- crates/miden-objects/src/lib.rs
+++ crates/miden-objects/src/lib.rs
@@ -24,8 +24,8 @@ mod errors;
 
 pub use constants::*;
 pub use errors::{
-    AccountDeltaError, AccountError, AccountIdError, AccountUpdateError, AssetError,
-    AssetVaultError, BlockError, ChainMmrError, NoteError, ProvenTransactionError,
+    AccountDeltaError, AccountError, AccountIdError, AssetError, AssetVaultError,
+    BatchAccountUpdateError, BlockError, ChainMmrError, NoteError, ProvenTransactionError,
     TransactionInputError, TransactionOutputError, TransactionScriptError,
 };
 pub use miden_crypto::hash::rpo::{Rpo256 as Hasher, RpoDigest as Digest};
