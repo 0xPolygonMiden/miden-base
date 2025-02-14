f3f64eece8d99cd9183adff716a502cb017018db
Philipp Gackstatter <PhilippGackstatter@users.noreply.github.com>
2025-02-14 13:25:52 +0100

    feat: Document partial nullifier tree methods

--- crates/miden-objects/src/block/partial_nullifier_tree.rs
+++ crates/miden-objects/src/block/partial_nullifier_tree.rs
@@ -7,25 +7,38 @@ use crate::{
     note::Nullifier,
 };
 
-/// The partial sparse merkle tree containing the nullifiers of consumed notes. This is the partial
-/// variant of [`NullifierTree`].
+/// The partial sparse merkle tree containing the nullifiers of consumed notes.
 pub struct PartialNullifierTree(PartialSmt);
 
 impl PartialNullifierTree {
     /// The leaf value of an unspent nullifier.
     pub const UNSPENT_NULLIFIER: Word = EMPTY_WORD;
 
+    /// Creates a new, empty partial nullifier tree.
     pub fn new() -> Self {
         PartialNullifierTree(PartialSmt::new())
     }
 
-    // TODO: Document errors.
+    /// Adds the given nullifier witness to the partial tree and tracks it. Once a nullifier has
+    /// been added to the tree, it can be marked as spent using [`Self::mark_spent`].
+    ///
+    /// # Errors
+    ///
+    /// Returns an error if:
+    /// - after the witness' merkle path was added the partial nullifier tree has a different root
+    ///   than before it was added.
     pub fn add_nullifier_witness(&mut self, witness: NullifierWitness) -> Result<(), MerkleError> {
         let (path, leaf) = witness.into_proof().into_parts();
         self.0.add_path(leaf, path)
     }
 
-    // TODO: Document errors.
+    /// Marks the given nullifier as spent at the given block number.
+    ///
+    /// # Errors
+    ///
+    /// Returns an error if:
+    /// - the nullifier is not tracked by this partial nullifier tree, that is, its
+    ///   [`NullifierWitness`] was not added to the tree previously.
     pub fn mark_spent(
         &mut self,
         nullifier: Nullifier,
@@ -34,6 +47,7 @@ impl PartialNullifierTree {
         self.0.insert(nullifier.inner(), block_num_to_leaf_value(block_num)).map(|_| ())
     }
 
+    /// Returns the root of the tree.
     pub fn root(&self) -> Digest {
         self.0.root()
     }
@@ -49,6 +63,6 @@ impl Default for PartialNullifierTree {
 // ================================================================================================
 
 /// Returns the nullifier's leaf value in the SMT by its block number.
-pub(super) fn block_num_to_leaf_value(block: BlockNumber) -> Word {
+fn block_num_to_leaf_value(block: BlockNumber) -> Word {
     [Felt::from(block), Felt::ZERO, Felt::ZERO, Felt::ZERO]
 }
