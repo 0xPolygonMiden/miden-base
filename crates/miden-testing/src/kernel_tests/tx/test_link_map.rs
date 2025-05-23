92349aa47e8c58f3269c3525b2028f488b03949a
Philipp Gackstatter <PhilippGackstatter@users.noreply.github.com>
2025-05-23 14:54:50 +0200

    feat: Implement `get` and `compute_commitment`

--- crates/miden-testing/src/kernel_tests/tx/test_link_map.rs
+++ crates/miden-testing/src/kernel_tests/tx/test_link_map.rs
@@ -4,15 +4,21 @@ use miden_objects::ONE;
 use crate::TransactionContextBuilder;
 
 #[test]
-fn set() -> anyhow::Result<()> {
+fn new_set_get() -> anyhow::Result<()> {
     let code = r#"
       use.kernel::link_map
 
       const.MAP_PTR=8
 
       begin
+          # Initialize a new map
+          # ---------------------------------------------------------------------------------------
+
           push.MAP_PTR exec.link_map::new
           # => []
+
+          # Insert a key-value pair
+          # ---------------------------------------------------------------------------------------
 
           # value
           push.1.2.3.4
@@ -25,6 +31,10 @@ fn set() -> anyhow::Result<()> {
           # => [OLD_VALUE]
 
           padw assert_eqw.err="old value should be the empty word"
+          # => []
+
+          # Overwrite the previously inserted key-value pair
+          # ---------------------------------------------------------------------------------------
 
           # value
           push.9.8.7.6
@@ -38,6 +48,29 @@ fn set() -> anyhow::Result<()> {
 
           push.1.2.3.4
           assert_eqw.err="old value should be the previously inserted value"
+          # => []
+
+          # Get the value at the previously inserted key
+          # ---------------------------------------------------------------------------------------
+
+          # key
+          push.5.6.7.8
+          push.MAP_PTR
+          # => [map_ptr, KEY]
+
+          exec.link_map::get
+          # => [VALUE]
+
+          push.9.8.7.6
+          assert_eqw.err="retrieved value should be the previously inserted value"
+          # => []
+
+          # Compute the map commitment
+          # ---------------------------------------------------------------------------------------
+
+          # for now we only assert that a value is returned
+          push.MAP_PTR exec.link_map::compute_commitment
+          # => [MAP_COMMITMENT]
       end
     "#;
 
